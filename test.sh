719bfd3e5cc6af7d2028b6e7513764d8271c8b20
hackorama <kishan.thomas@gmail.com>
2018-06-23 14:47:01 -0700

    Adding test run script

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Test helper script for AnyOption
+#
+# Requires: cppcheck valgrind lcov 
+#
+# Kishan Thomas <kishan.thomas@gmail.com>
+
+cppcheck --error-exitcode=1 --enable=warning,performance,information,style *.cpp *.h
+wget -q https://github.com/catchorg/Catch2/releases/download/v2.2.3/catch.hpp
+g++ -g -o0 -coverage -std=c++11 -Wall -Wextra -Werror test.cpp anyoption.cpp -o test
+valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./test
+lcov --directory . --capture --output-file coverage.info
+lcov --remove coverage.info 'catch.hpp' '/usr/*' --output-file coverage.info
+lcov --list coverage.info
+# clean
+rm test
+rm catch.hpp
+rm coverage.info
+rm *.gcda *.gcno
